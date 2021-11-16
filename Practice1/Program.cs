b6167510aa3eed53bdb0f23dc47443fa880d8bd1
Aieos <firered9000@yahoo.com>
2021-11-15 23:01:02 -0600

    Initial commit

---
+++ Practice1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Practice1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Registration Form ");
+            Console.WriteLine();
+            Console.Write("First name: ");
+            string firstname = Console.ReadLine();
+            Console.Write("Last name: ");
+            string lastname = Console.ReadLine();
+            Console.Write("Birth year: ");
+            string birthyear = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Welcome " + firstname + " " + lastname);
+            Console.WriteLine("Your registration is complete.");
+            Console.WriteLine("Your temporary password is: " + firstname + "*" + birthyear);
+        }
+    }
+}
